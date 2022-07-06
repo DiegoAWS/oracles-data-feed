fc4e85afc8957b18248ba9bfff52f22c3d616fc4
Diego Escobar <diego.netsolutions@gmail.com>
2022-07-07 01:32:44 +0300

    updated functions

--- netlify/functions/getPairs.js
+++ netlify/functions/getPairs.js
@@ -1,18 +1,38 @@
 const axios = require('axios');
 
 exports.handler = async function (event, context) {
-    const apiKey = process.env.REACT_APP_CRYPTOWATCH_PUBLIC_KEY;
 
-    return axios.get(`https://api.cryptowat.ch/pairs?apikey=${apiKey}`)
+    const url = event?.headers && event?.headers['x-url']
+    if (!url) {
+        return {
+            statusCode: 400,
+            headers: {
+                "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+            },
+            body: JSON.stringify({
+                error: `No url provided, Headers['x-url'] is missing`
+            })
+        }
+    }
+    return axios.get(url)
         .then(response => {
             return {
                 statusCode: 200,
-                body: JSON.stringify(response.data)
+                headers: {
+                    "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                },
+                body: JSON.stringify({
+                    data: response.data,
+                 
+                })
             }
         }).catch(error => {
             console.log(error)
             return {
-                statusCode: 422,
+                statusCode: 422,  
+                headers: {
+                    "Access-Control-Allow-Origin": "*", // Allow from anywhere 
+                },
                 body: `Error: ${error}`,
             }
         })
