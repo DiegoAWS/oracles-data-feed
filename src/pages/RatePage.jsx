83a909acc513e79260c638beb36876f45f17d7b2
DiegoAWS <diego.netsolutions@gmail.com>
2022-07-28 11:08:08 +0300

    updated rate pages

--- src/pages/RatePage.jsx
+++ src/pages/RatePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
+import { Brush, CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 import ExchangeSelector from '../components/ExchangeSelector/ExchangeSelector'
 import TabsSector from '../components/TabsSector/TabsSector'
 import './RatePage.scss'
@@ -23,13 +23,36 @@ function RatePage() {
                             top: 5,
                             right: 30,
                             left: 20,
-                            bottom: 5,
+                            bottom: 25,
                         }}
                     >
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="name" />
                         <YAxis />
                         <Tooltip />
+                        <Brush />
+                        <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                        <Line type="natural" dataKey="amt" stroke="red" />
+                    </LineChart>
+                </ResponsiveContainer>
+                <ResponsiveContainer width="100%" height="100%">
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 25,
+                            right: 30,
+                            left: 20,
+                            bottom: 25,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Brush />
                         <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
                         <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
                         <Line type="natural" dataKey="amt" stroke="red" />
